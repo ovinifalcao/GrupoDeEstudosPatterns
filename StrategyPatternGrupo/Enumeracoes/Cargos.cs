9270ef796123115ffc81b0dcbc15da5976f3ade5
Vinícius Falcão <vinicius.falcao@lambda3.com.br>
2022-10-18 19:22:05 -0300

    Adição do método para a enumeração.

---
+++ StrategyPatternGrupo/Enumeracoes/Cargos.cs
@@ -0,0 +1,20 @@
+﻿using StrategyPatternGrupo.Implementacoes;
+
+namespace StrategyPatternGrupo.Enumeracoes
+{
+    public enum Cargos
+    {
+        Desenvolvedor,
+        DBA,
+        Tester
+    }
+
+    public static class ExtenssoesCargos
+    {
+        public static IRegraDeCalculo regraDeCalculo(this Cargos cargo)
+        {
+            if (cargo == Cargos.Desenvolvedor) return new CalculadoraDesenvolvedor();
+            return new CalculadoraGeral();
+        } 
+    }
+}
